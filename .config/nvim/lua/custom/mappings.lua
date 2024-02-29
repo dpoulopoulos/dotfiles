0606f4896aee9aed9d42a41af95ac9e72f1a0741
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-02-29 15:20:42 +0200

    Use the alt key to navigate between tmux panes
    Using the `ctrl` key to navigate between tmux and Vim panes presents a
    problem: `ctrl + l` is the key to clear the terminal.
    
    Rebind the navigation mappings to use the `alt` key instead.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.general = {
   n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    ["<M-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<M-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<M-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<M-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
   }
 }
 
