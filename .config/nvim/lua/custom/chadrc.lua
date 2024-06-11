fabd2799cb956de5ab2d7e66eda3bd108b447d87
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-06-11 10:34:09 +0300

    nvim: Delete NvChad config
    The new version of Neovim's NvChad distribution uses a new a way of
    configuring Neovim.
    
    Delete the NvChad configuration as it is now useless.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,6 +0,0 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = "catppuccin", statusline = {theme="vscode"}}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
